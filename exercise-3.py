78063b619041a61878efb3be168cbcba6afdca89
sopheakPHOEUN <2024021phoeun@aupp.edu.kh>
2024-03-17 16:18:50 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,10 +1,9 @@
 def remove_all_after(numbers, n):
-    ...
+    # check the element if n in the list 
     if n in numbers:
+    # if n in the list, return element from the first to the element that user input 
         return numbers[:numbers.index(n) + 1]
+    # if n is not in the list, return the original list 
     return numbers
-numbers = [1, 1, 2, 2, 3, 3]
-n = int(input("n = "))
-result = remove_all_after(numbers, n)
-print(f"numbers: {result}")
+
 
