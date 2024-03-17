96222575a05c755e53b57c151de0143ac9fdaf51
sopheakPHOEUN <2024021phoeun@aupp.edu.kh>
2024-03-17 16:09:28 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,10 @@
 def remove_all_after(numbers, n):
     ...
+    if n in numbers:
+        return numbers[:numbers.index(n) + 1]
+    return numbers
+numbers = [1, 1, 2, 2, 3, 3]
+n = int(input("n = "))
+result = remove_all_after(numbers, n)
+print(f"numbers: {result}")
+
